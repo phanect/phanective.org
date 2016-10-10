ad776860db1e0c4009dc86605093e8bd9b8d61d4
Jumpei Ogawa <phanective@gmail.com>
2016-10-10 23:18:50 +0900

    tmp: Add projects section

---
+++ elements/ph-project/ph-project.js
@@ -0,0 +1,21 @@
+(function() {
+  "use strict";
+
+  Polymer({
+    is:         "ph-project",
+    properties: {
+      name: {
+        type: String
+      },
+      url: {
+        type: String
+      },
+      start: {
+        type: String
+      },
+      end: {
+        type: String
+      }
+    }
+  });
+}());
