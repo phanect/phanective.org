88aa015b645c40526a8fc7b8538edc1dda789698
Jumpei Ogawa <phanective@gmail.com>
2017-06-06 17:45:09 +0900

    Separate build process to `npm run build`

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,7 +13,7 @@ rm --recursive --force ./npm_modules
 yarn install
 npm run build
 
-mkdir .build
+mkdir --parent .build
 cp --recursive ./.git ./.build/
 
 cd .build
@@ -21,8 +21,6 @@ cd .build
 git fetch origin
 git checkout --force gh-pages
 git reset --hard origin/gh-pages
-
-harp compile ../ ./
 
 git add --all
 git commit --message="Deployment at $(date "+%F %H:%M:%S")"
