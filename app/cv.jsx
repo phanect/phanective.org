2174db25e31c6113dd8d5f87d4e78f325ab2839b
Jumpei Ogawa <phanective@gmail.com>
2017-06-07 00:30:30 +0900

    Replace skills section using react-grid-layout

--- app/cv.jsx
+++ app/cv.jsx
@@ -1,10 +1,27 @@
 "use strict";
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGridLayout from "react-grid-layout";
+
+const jsSkillsGrid = React.createClass({
+  render: () => {
+    // TODO
+    var layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+    return (
+      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key={'a'}>a</div>
+        <div key={'b'}>b</div>
+        <div key={'c'}>c</div>
+      </ReactGridLayout>
+    )
+  }
+});
 
 ReactDOM.render(
-  <span>
-    Hello
-  </span>,
+  <jsSkillsGrid></jsSkillsGrid>,
   document.getElementById("skills-js")
 );
