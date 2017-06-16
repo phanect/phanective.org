5bb52a2ef22d53effa9f671a6111bca3fa2514c3
Jumpei Ogawa <phanective@gmail.com>
2017-06-17 00:15:03 +0900

    tmp

--- app/cv.jsx
+++ app/cv.jsx
@@ -1,27 +1,42 @@
 "use strict";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactGridLayout from "react-grid-layout";
+import { Responsive as ResponsiveReactGridLayout } from "react-grid-layout";
 
-const jsSkillsGrid = React.createClass({
-  render: () => {
-    // TODO
-    var layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
-    ];
+const skills = [
+    { id: "javascript", name: "JavaScript", png: true },
+    { id: "nodejs", name: "Node.js" },
+    { id: "typescript", name: "TypeScript" },
+    { id: "polymer", name: "Polymer" },
+    { id: "angular", name: "Angular" },
+    { id: "vuejs", name: "Vue.js" },
+    { id: "geddy", name: "Geddy", png: true },
+    { id: "webpack", name: "Webpack", png: true },
+    { id: "mocha", name: "Mocha" },
+    { id: "webdriverio", name: "WebDriver.io", png: true },
+    { id: "stylus", name: "Stylus" },
+    { id: "css", name: "CSS" },
+    { id: "html5", name: "HTML5" },
+    { id: "jquery", name: "jQuery" },
+  ],
+  layout = skills.map((skill, i) => {
+    return { i: skill.id, x: i, y: 0, w: 2, h: 2, static: true };
+  }),
+  itemDivs = skills.map((skill) => {
     return (
-      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key={'a'}>a</div>
-        <div key={'b'}>b</div>
-        <div key={'c'}>c</div>
-      </ReactGridLayout>
-    )
-  }
-});
+      <div key={skill.id} className="technology">
+        <img src={"/images/technologies/" + skill.id + "." + (skill.png ? "png" : "svg") } alt={skill.name} />
+        <p>{skill.name}</p>
+      </div>
+    );
+  });
 
 ReactDOM.render(
-  <jsSkillsGrid></jsSkillsGrid>,
+  <ResponsiveReactGridLayout layout={layout} width={2400} isDraggable={false} isResizable={false}
+    breakpoints={{lg: 2400, md: 2400, sm: 2400, xs: 2400, xxs: 2400}}
+    cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}>
+    {itemDivs}
+  </ResponsiveReactGridLayout>,
   document.getElementById("skills-js")
 );
