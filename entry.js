8adc88b899337394c5639e25904379e2ea3f45b8
Jumpei Ogawa <jumpei.ogawa@spelldata.co.jp>
2018-11-07 12:49:12 +0900

    testing

--- entry.js
+++ entry.js
@@ -1,3 +1,6 @@
 "use strict";
 
-import"./style.css";
+import "./style.css";
+import { upgradeElement } from "node_modules/@ampproject/worker-dom/dist/index.mjs";
+
+upgradeElement(document.getElementById("upgrade-me"), "/node_modules/@ampproject/worker-dom/dist/worker.js");
